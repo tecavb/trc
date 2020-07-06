b0a9aa719d84ed64cbad7e014fdfa9464f9964b7
mingxuan <850529820@qq.com>
2020-07-06 18:44:28 +0800

    v

--- Trc.js
+++ Trc.js
@@ -1,6 +1,7 @@
 class Tcr {
     constructor(val, all) {//val:可以是字符串,也可以是对象，但必须含有key这个键名，all:大杂烩，包含所有节点，构建实例的时候必须传进去
-        let temp = Object.prototype.toString.call(val) == '[object Object]'
+        let temp = Object.prototype.toString.call(val) == '[object Object]';
+        debugger;
         if (temp) {
             if (!val.key) {
                 throw Error('The object must contain the key name "key"')
@@ -10,6 +11,7 @@ class Tcr {
         } else {
             throw Error('This argument can only be a string or an object')
         }
+        this.$val = val;
         all.add(this)
         this.$all = all;
     }
@@ -43,12 +45,33 @@ class Tcr {
         !key ? this.forEach(item => { temp = item }) : null
         return this[key] ? this[key] : temp//当前实例存在目标是移动到目标，不存在时返回原实例
     }
-    find(tar) {
-        let temp = undefined;
+    find(tar) {//在all中找目标
+        let temp = [];
         this.$all.forEach(item => {
-            item.$val.key == tar ? temp = item : null;
+            item.$val.key == tar ? temp.push(item) : null;
         })
         return temp
+    }
+    static create(obj) {//直接传入对象，就可以直接生成Tcr链
+        !obj && console.log('e.g:{key1:[val1,val2,val3],key2:[val2,val3,val4]}');
+        let ary = {}, all = new Set;
+        Object.keys(obj).forEach(item => {
+            obj[item].forEach(it => {
+                typeof it == 'object' ? it = it.key : null
+                let temp = Object.keys(ary).some(item => {
+                    return ary[item].$val.key == it
+                })
+                temp ? null : ary[it] = new Tcr(it, all);
+            })
+        })
+        Object.keys(obj).forEach(item => {
+            let temp = new Tcr(item, all)
+            obj[item].forEach(it => {
+                typeof it == 'object' ? it = it.key : null
+                temp.add(ary[it])
+            })
+        })
+        return all
     }
 }
 export default Tcr
--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-import Trc from "./Trc.js";
-console.log(Trc)
+import Tcr from "./Trc.js";
+// let all = new Set();
+// let e1 = new Tcr('1',all);
+// let e2 = new Tcr('2',all);
+// let e3 = new Tcr('3',all);
+// let e4 = new Tcr('4',all);
+// let e5 = new Tcr('5',all);
+// let e6 = new Tcr('6',all);
+// new Tcr('A',all).add(e1,e2,e3,e4);
+// new Tcr('B',all).add(e1,e4,e5,e6);
+// new Tcr('C',all).add(e3,e4,e6);
+// new Tcr('D',all).add(e2,e1,e5);
+// new Tcr('E',all).add(e5,e6);
+// new Tcr('F',all).add(e2,e3);
+let n = Tcr.create({ A: [{ ke: '1' }, '2', '3'], B: ['2', { key: '3' }, '4'], C: ['3', '4', '5'] })
+console.log(n)
